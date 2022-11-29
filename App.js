a8782a0c9df58250b6cf3513206af7ead8268337
Nifia <2001020003@student.umrah.ac.id>
2022-11-30 01:37:27 +0700

    tambah-detailPage

--- App.js
+++ App.js
@@ -1,20 +1,261 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ImageStore } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
 
-export default function App() {
+import { images, icons, COLORS, FONTS, SIZES } from './constants';
+
+const StarReview = ({ rate }) => {
+  var starComponents = [];
+  var fullStar = Math.floor(rate)
+  var noStar = Math.floor(5 - rate)
+  var halfStar = 5 - fullStar - noStar
+
+  // Full Star
+  for (var i = 0; i < fullStar; i++) {
+      starComponents.push(
+          <Image
+              key={`full-${i}`}
+              source={icons.starFull}
+              resizeMode="cover"
+              style={{
+                  width: 20,
+                  height: 20,
+              }}
+          />
+      )
+  }
+
+  // Half Star
+  for (var i = 0; i < halfStar; i++) {
+      starComponents.push(
+          <Image
+              key={`half-${i}`}
+              source={icons.starHalf}
+              resizeMode="cover"
+              style={{
+                  width: 20,
+                  height: 20,
+              }}
+          />
+      )
+  }
+
+  // No Star
+  for (var i = 0; i < noStar; i++) {
+      starComponents.push(
+          <Image
+              key={`empty-${i}`}
+              source={icons.starEmpty}
+              resizeMode="cover"
+              style={{
+                  width: 20,
+                  height: 20,
+              }}
+          />
+      )
+  }
+
+  return (
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          {starComponents}
+          <Text style={{ marginLeft: SIZES.base, color: COLORS.gray, ...FONTS.body3 }}>{rate}</Text>
+      </View>
+  )
+}
+
+const IconLabel = ({ icon, label }) => {
+  return (
+      <View style={{ alignItems: 'center' }}>
+          <Image
+              source={icon}
+              resizeMode="cover"
+              style={{
+                  width: 50,
+                  height: 50,
+              }}
+          />
+          <Text style={{ marginTop: SIZES.padding, color: COLORS.gray, ...FONTS.h3 }}>{label}</Text>
+      </View>
+  )
+}
+
+
+export default function App({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      {/* Header */}
+      <View style={{ flex: 2 }}>
+        <Image 
+          source={{uri: 'https://i0.wp.com/bersamawisata.com/wp-content/uploads/2021/07/488600_18071812270066954646.jpg?fit=1024%2C768&ssl=1'}}
+          style={{
+            width: 400, 
+            height: 300
+            }} 
+        />
+
+        <View
+          style={[{
+            position: 'absolute',
+            bottom: '5%',
+            left: '5%',
+            right: '5%',
+            borderRadius: 15,
+            padding: SIZES.padding,
+            backgroundColor: '#fff',
+          }, styles.shadow]}
+        >
+            <View style={{ flexDirection: 'row'}}>
+                <View style={styles.shadow}>
+                  <Image
+                    source={{uri: 'https://i0.wp.com/bersamawisata.com/wp-content/uploads/2021/07/488600_18071812270066954646.jpg?fit=1024%2C768&ssl=1'}}
+                    style={{
+                      width: 70, 
+                      height: 70,
+                      borderRadius: 15
+                      }} 
+                  />
+                </View>
+
+                <View style={{marginHorizontal: 20, justifyContent: 'space-around'}}>
+                  <Text style={{fontWeight: 'bold'}}>Rose Garden Hotel</Text>
+                  <Text style={{color: 'gray'}}>San Bruno, California</Text>
+
+                  <StarReview
+                    rate={4.5}
+                  />
+
+                </View>
+              </View> 
+
+              <View style={{ marginTop: SIZES.radius }}>
+                        <Text style={{ color: COLORS.gray, ...FONTS.body3 }}>
+                        The Hotel Rose Garden is San Jose's newest independent hotel
+                        </Text>
+              </View>
+        </View>
+
+        {/* Header Buttons */}
+        <View
+        style={{
+          position: 'absolute',
+          top: 50,
+          left: 20,
+          right: 20,
+          //height: 50,
+          flexDirection: 'row',
+      }}
+  >
+      <View style={{ flex: 1 }}>
+          <TouchableOpacity
+              onPress={() => { navigation.navigate('Home') }}
+          >
+              <Image
+                  source={icons.back}
+                  resizeMode="cover"
+                  style={{
+                      width: 30,
+                      height: 30,
+                  }}
+              />
+          </TouchableOpacity>
+      </View>
+      <View style={{ flex: 1, alignItems: 'flex-end' }}>
+          <TouchableOpacity
+              onPress={() => { console.log("Menu on pressed") }}
+          >
+              <Image
+                  source={icons.menu}
+                  resizeMode="cover"
+                  style={{
+                      width: 30,
+                      height: 30,
+                  }}
+              />
+          </TouchableOpacity>
+      </View>
+  </View>
+                    
+      </View>
+
+      {/* Body */}
+      <View style={{ flex: 1.5 }}>
+        {/* Icons */}
+        <View style={{paddingHorizontal: SIZES.padding }}>
+          <Text style={{ ...FONTS.h2 }}>Facilities</Text>
+        </View>
+        <View style={{ flexDirection: 'row', marginTop: SIZES.base, paddingHorizontal: SIZES.padding * 2, justifyContent: 'space-between' }}>
+                    <IconLabel
+                        icon={icons.wifi}
+                        label="Villa"
+                    />
+
+                    <IconLabel
+                        icon={icons.parking}
+                        label="Parking"
+                    />
+
+                    <IconLabel
+                        icon={icons.spa}
+                        label="Spa"
+                    />
+                </View>
+
+                {/* About */}
+                <View style={{ marginTop: SIZES.padding, paddingHorizontal: SIZES.padding }}>
+                    <Text style={{ ...FONTS.h2 }}>About</Text>
+                    <Text style={{ marginTop: SIZES.radius, color: COLORS.gray, ...FONTS.body3 }}>
+                      Located on The Alameda off of Interstate I-880, The Hotel Rose Garden is located in The Heart of Silicon Valley.
+                    </Text>
+                </View>
+      </View>
+
+      {/* Footer */}
+      <View style={{ flex: 0.5, paddingHorizontal: SIZES.padding }}>
+                <LinearGradient
+                    style={[{ height: 70, width: '100%', borderRadius: 15 }]}
+                    colors={['#edf0fc', '#d6dfff']}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                >
+                    <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+                        <View style={{ flex: 1, marginHorizontal: SIZES.padding, justifyContent: 'center' }}>
+                            <Text style={{ ...FONTS.h1 }}>$1000</Text>
+                        </View>
+
+                        <TouchableOpacity
+                            style={{ width: 130, height: '80%', marginHorizontal: SIZES.radius }}
+                            onPress={() => { console.log("Booking on pressed") }}
+                        >
+                            <LinearGradient
+                                style={[{ flex: 1, alignItems: 'center', justifyContent: 'center', borderRadius: 10 }]}
+                                colors={['#7FE9DE', '#FF3CBD']}
+                                start={{ x: 0, y: 0 }}
+                                end={{ x: 1, y: 0 }}
+                            >
+                                <Text style={{ color: COLORS.white, ...FONTS.h2 }}>BOOKING</Text>
+                            </LinearGradient>
+                        </TouchableOpacity>
+                    </View>
+                </LinearGradient>
+            </View>
+        </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.35,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
 });
